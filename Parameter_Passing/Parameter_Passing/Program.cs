f8de76cef63c71344742049bb1df4cf4f8d75469
VaishnaviPatil753 <114859651+VaishnaviPatil753@users.noreply.github.com>
2022-10-28 22:16:04 +0530

    Add files via upload

---
+++ Parameter_Passing/Parameter_Passing/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Parameter_Passing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arg;
+            arg = 4;
+            squareval(arg);
+            Console.WriteLine(arg);
+            Console.ReadKey();
+        }
+        static void squareval(int valParameter)
+        {
+            valParameter *= valParameter;
+
+        }
+    }
+}
