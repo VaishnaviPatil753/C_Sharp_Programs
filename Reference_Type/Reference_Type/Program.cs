242f62b5087190fd21505a44582b0cd35656d7b1
VaishnaviPatil753 <114859651+VaishnaviPatil753@users.noreply.github.com>
2022-10-30 17:55:08 +0530

    Add files via upload

---
+++ Reference_Type/Reference_Type/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Reference_Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arg;
+            arg = 5;
+            squareRef(ref arg);
+            Console.WriteLine(arg);
+            Console.ReadKey();
+        }
+        static void squareRef(ref int refParameter)
+        {
+            refParameter *= refParameter;
+
+        }
+    }
+}
